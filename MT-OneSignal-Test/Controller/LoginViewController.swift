86dde6f4d6883f2b6ad375ddde1b5adccec122da
MattTripodi <mtripodi1027@gmail.com>
2024-01-28 14:55:09 -0500

    add IBOutlets and IBActions on loginViewController

--- MT-OneSignal-Test/Controller/LoginViewController.swift
+++ MT-OneSignal-Test/Controller/LoginViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var loginButton: UIButton!
+    @IBOutlet var skipLoginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "toMain", sender: self)
+    }
+    
+    @IBAction func skipLoginButtonTapped(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "toMain", sender: self)
+    }
 }
 
--- MT-OneSignal-Test/View/RoundButton.swift
+++ MT-OneSignal-Test/View/RoundButton.swift
@@ -5,4 +5,16 @@
 //  Created by Matthew Tripodi on 1/28/24.
 //
 
-import Foundation
+import UIKit
+
+class RoundedButton: UIButton {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = #colorLiteral(red: 0.8949897885, green: 0.2875944376, blue: 0.2848131657, alpha: 1)
+    }
+    
+}
