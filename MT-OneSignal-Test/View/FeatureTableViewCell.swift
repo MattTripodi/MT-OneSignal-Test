f862e29b0fe10a0bcee1d60adf0f76f40875bbf2
MattTripodi <mtripodi1027@gmail.com>
2024-01-29 13:58:28 -0500

    Design MainViewController UI, implement tableView with features

---
+++ MT-OneSignal-Test/View/FeatureTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeatureTableViewCell.swift
+//  MT-OneSignal-Test
+//
+//  Created by Matthew Tripodi on 1/29/24.
+//
+
+import UIKit
+
+class FeatureTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
